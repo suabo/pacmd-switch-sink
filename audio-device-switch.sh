42ca37c7dfda73ac4965ff5bace69f67fcff1800
Marcel Grolms <info@suabo.de>
2022-11-18 23:15:03 +0100

    - removed warnings and errors from shellcheck - renamed vars to follow one style - show available sink ids on usage output

--- audio-device-switch.sh
+++ audio-device-switch.sh
@@ -17,89 +17,117 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+all_sinks=()
+all_sink_ids=()
+available_sink_ids=()
+ignore_sink_ids=()
+ignore_sinks=()
+
 notify=0
-while getopts d:nh flag
+
+declare -i sinks_count
+declare -i active_sink_index
+
+# check for available sinks
+sinks_count=$(pacmd list-sinks | grep -c "index:[[:space:]][[:digit:]]")
+if [ "$sinks_count" -eq 0 ] ; then
+    echo "No sink detected."
+    exit
+fi
+# set active sink index
+active_sink_index=$(pacmd list-sinks | sed -n -e 's/[[:space:]][[:space:]]\*[[:space:]]index:[[:space:]]\([[:digit:]]\)/\1/p')
+
+#### FUNCTION BEGIN
+# Set all and available sinks
+# GLOBALS: 
+#   all_sinks all_sink_ids available_sink_ids ignore_sink_ids ignore_sinks
+### FUNCTION END
+function get_available_sinks {
+    while read -r row ; 
+    do
+        IFS=' ' read -r -a current_sink <<< "$row"
+        #echo "Index: ${current_sink[0]} Vendor: ${current_sink[1]} Product: ${current_sink[2]}"
+        all_sink_ids+=("${current_sink[0]}")
+        all_sinks+=("${current_sink[1]}:${current_sink[2]}")
+
+        for sink in "${ignore_sink_ids[@]}"; do
+            if [ "${current_sink[1]}:${current_sink[2]}" == "$sink" ] ; then
+                #echo "Exclude sink with index $sink from switching"
+                ignore_sinks+=("${current_sink[0]}")
+            fi
+        done
+    done <<<$(pacmd list-sinks | sed -n -e "s/[\ \*]*index:\ \([[:digit:]]\)\|[\  \t]*device\.product\.id\ \=\ \"\([0-9a-f]\{4\}\)\"\|[\ \t]*device\.vendor\.id\ =\ \"\(....\)\"\|[\ \t]*\(ports:\)/\1\2\3\4/p" | awk -v RS='ports:\n' -v FS='\n' -v ORS='\n' -v OFS=' ' '{ $1=$1 };1')
+
+    for index in "${all_sink_ids[@]}"; do
+        if [ "${#ignore_sinks[@]}" -ne 0 ] ; then
+            for ignoreSinkIndex in "${ignore_sinks[@]}"; do
+                if [ "$index" -ne "$ignoreSinkIndex" ] ; then
+                    available_sink_ids["${#available_sink_ids[@]}"]="$index"
+                fi
+            done
+        else
+            available_sink_ids[${#available_sink_ids[@]}]=$index
+        fi
+    done
+}
+
+while getopts i:nh flag
 do
     case "${flag}" in
-        d) disable=${OPTARG};;
-        n) notify=1;;
-        *) echo "Usage: audio-device-switch -d \"vendor:productId vendor:productId\""; exit;;
+        i) 
+            ignore=${OPTARG}
+            IFS=' ' read -r -a ignore_sink_ids <<< "$ignore"
+            ;;
+        n) 
+            notify=1
+            ;;
+        *) 
+            echo "Usage: audio-device-switch [-i \"vendor:productId vendor:productId ...\"]" >&2
+            get_available_sinks
+            echo "  Available sinks: " >&2
+            for sink in "${all_sinks[@]}"; do
+                echo "      $sink"
+            done
+            exit
+            ;;
     esac
 done
 
-declare -i sinks_count=`pacmd list-sinks | grep -c index:[[:space:]][[:digit:]]`
-if [ $sinks_count -eq 0 ] ; then
-    echo "No audio output detected."
-    exit
-fi
-
-IFS=' ' read -r -a disableSink <<< "$disable"
-
-arrSinkIndex=()
-arrDisabledSinkIndex=()
-
-while read row ; 
-do
-    IFS=' ' read -r -a arrSink <<< "$row"
-    #echo "Index: ${arrSink[0]} Vendor: ${arrSink[1]} Product: ${arrSink[2]}"
-    loopSink="${arrSink[1]}:${arrSink[2]}"
-
-    for sink in ${disableSink[@]}; do
-        if [ $loopSink == $sink ] ; then
-            #echo "Exclude sink with index $sink from switching"
-            arrDisabledSinkIndex+=(${arrSink[0]})
-        fi
-    done
-done <<<$(pacmd list-sinks | sed -n -e "s/[\ \*]*index:\ \([[:digit:]]\)\|[\  \t]*device\.product\.id\ \=\ \"\([0-9a-f]\{4\}\)\"\|[\ \t]*device\.vendor\.id\ =\ \"\(....\)\"\|[\ \t]*\(ports:\)/\1\2\3\4/p" | awk -v RS='ports:\n' -v FS='\n' -v ORS='\n' -v OFS=' ' '{ $1=$1 };1')
-
-declare -i active_sink_index=`pacmd list-sinks | sed -n -e 's/[[:space:]][[:space:]]\*[[:space:]]index:[[:space:]]\([[:digit:]]\)/\1/p'`
-
-while read index ;
-do
-    if [ ${#a[@]} -ne 0 ] ; then
-        for disableSinkIndex in ${arrDisabledSinkIndex[@]}; do
-            if [ $index -ne $disableSinkIndex ] ; then
-                arrSinkIndex[${#arrSinkIndex[@]}]=$index
-            fi
-        done
-    else
-        arrSinkIndex[${#arrSinkIndex[@]}]=$index
-    fi
-done < <(pacmd list-sinks | sed -n -e 's/[\*\ ]*index:[[:space:]]\([[:digit:]]\)/\1/p')
+get_available_sinks
 
 active_index_position_found=0
-let next_sink_index=-1
+(( next_sink_index=-1 ))
 
-for index in ${arrSinkIndex[@]}; do
-  if [ $next_sink_index -lt 0 ] ; then
+for index in "${available_sink_ids[@]}"; do
+    if [ $next_sink_index -lt 0 ] ; then
         export next_sink_index=$index
     fi
+    # if active sink is found this will be the next
     if [ $active_index_position_found -eq 1 ] ; then
-        if [[ ! "${arrDisabledSinkIndex[*]}" =~ "${index}" ]]; then
-            export next_sink_index=$index
-            break;
-        fi
+        export next_sink_index=$index
+        break;
     fi
-    if [ $active_sink_index -eq $index ] ; then
+    # find active sink
+    if [ "$active_sink_index" -eq "$index" ] ; then
         export active_index_position_found=1
     fi  
 done
 
-#change the default sink
+# change the default sink
 pacmd "set-default-sink ${next_sink_index}"
 
-#move all inputs to the new sink
+# move all inputs to the new sink
 for app in $(pacmd list-sink-inputs | sed -n -e 's/index:[[:space:]]\([[:digit:]]\)/\1/p');
 do
     pacmd "move-sink-input $app $next_sink_index"
 done
 
-#display notification
+# display notification
 if [ $notify -ne 0 ] ; then
     ndx=0
-    pacmd list-sinks | sed -n -e 's/device.description[[:space:]]=[[:space:]]"\(.*\)"/\1/p' | while read line;
+    pacmd list-sinks | sed -n -e 's/device.description[[:space:]]=[[:space:]]"\(.*\)"/\1/p' | while read -r line;
     do
-        if [ $next_sink_index -eq ${arrSinkIndex[$ndx]} ] ; then
+        if [ "$next_sink_index" -eq "${available_sink_ids[$ndx]}" ] ; then
             notify-send -i preferences-desktop-multimedia "Sound output switched to:" "$line"
             exit
         fi
